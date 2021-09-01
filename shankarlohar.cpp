e4a53d68c713e8af4092a051c2b1eb25c23e7061
Shankar Lohar <74100292+ShankarLohar@users.noreply.github.com>
2021-09-01 20:10:59 +0530

    Initial code template

---
+++ shankarlohar.cpp
@@ -0,0 +1,27 @@
+/*
+   Code Author : ShankarLohar 
+   https://www.linkedin.com/in/shankarlohar/
+*/
+
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define nl "\n"
+using namespace std;
+
+inline void debugMode(){
+    fastio;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main(){
+    debugMode();
+    int t;cin>>t;
+    while(t--){
+        
+    }
+        
+    return 0;
+}
