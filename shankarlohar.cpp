3e8d29fcf49f5cd67c706a4a56e391f4d77f91b6
Shankar Lohar <shankarloharmail@gmail.com>
2021-09-15 12:24:28 +0530

    Code Learning Update
    Learnt a new Algorithm to count the number of 1's in binary representation of a integer.

--- shankarlohar.cpp
+++ shankarlohar.cpp
@@ -16,6 +16,15 @@ inline void debugMode(){
     #endif
 }
 
+unsigned int countSetBits(int n){
+    unsigned int count = 0;
+    while (n){
+        n &= (n-1);
+        count++;
+    }
+    return count;
+}
+
 int main(){
     debugMode();
     int t;cin>>t;
