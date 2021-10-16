ab7c4e03d6dc147f1a5cad0eeaf47db66fc55879
Shankar Lohar <shankarloharmail@gmail.com>
2021-10-16 17:34:41 +0530

    Begginer-in-CP

---
+++ MyTemplate.cpp
@@ -0,0 +1,34 @@
+/*
+   Code Author : ShankarLohar 
+   https://www.linkedin.com/in/shankarlohar/
+*/
+
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+// typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+typedef priority_queue<int,vector<int>,greater<int>> minheap;
+typedef priority_queue<int> maxheap;
+typedef vector<int> intvector;
+typedef pair<int,int> intpair;
+typedef vector<pair<int,int>> vectorpair;
+typedef vector<tuple<int,int,int>> vectortuple;
+#define nl "\n"
+#define pushback push_back
+#define makepair make_pair
+#define forloop(i,a,b) for(int i = a; i<=b; i++)
+int main(){
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   #ifndef ONLINE_JUDGE
+   freopen("MyInput.txt", "r", stdin);
+   freopen("MyOutput.txt", "w", stdout);
+   #endif
+   /*Code Starts Here*/
+   /*=================================================*/
+   
+   
+   return 0;
+}
