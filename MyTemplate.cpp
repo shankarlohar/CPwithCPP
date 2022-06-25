58cbd7abf6c8cdb69f2b1525d382ee07c2fbceda
Shankar Lohar <shankarloharmail@gmail.com>
2022-06-25 14:53:12 +0530

    updated

--- MyTemplate.cpp
+++ MyTemplate.cpp
@@ -1,34 +1,58 @@
 /*
    Code Author : ShankarLohar 
-   https://www.linkedin.com/in/shankarlohar/
+   Connect On : https://www.linkedin.com/in/shankarlohar/
 */
 
 #include <bits/stdc++.h>
-// #include <ext/pb_ds/assoc_container.hpp>
-// using namespace __gnu_pbds;
 using namespace std;
-// typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
-typedef long long ll;
-typedef priority_queue<int,vector<int>,greater<int>> minheap;
-typedef priority_queue<int> maxheap;
-typedef vector<int> intvector;
-typedef pair<int,int> intpair;
-typedef vector<pair<int,int>> vectorpair;
-typedef vector<tuple<int,int,int>> vectortuple;
-#define nl "\n"
-#define pushback push_back
-#define makepair make_pair
-#define forloop(i,a,b) for(int i = a; i<=b; i++)
-int main(){
-   ios_base::sync_with_stdio(0);
-   cin.tie(0);
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+   const char* comma = strchr (names + 1, ',');
+   cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+
+// Solving the problem here...
+void solve(){
+
+}
+
+
+int32_t main(){
+   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
    #ifndef ONLINE_JUDGE
    freopen("MyInput.txt", "r", stdin);
    freopen("MyOutput.txt", "w", stdout);
    #endif
-   /*Code Starts Here*/
-   /*=================================================*/
-   
-   
+   clock_t z = clock();
+   int t = 1;
+   // cin >> t;
+   while (t--) solve();
+   cerr << "Run Time :" << ((double)(clock() - z)/CLOCKS_PER_SEC);
    return 0;
 }
